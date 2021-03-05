81bcd9211406f9101608d82512e442399bdfdd81
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-03-05 10:47:00 -0600

    Fetched Data from json placeholder

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ data }) {
 
   const robots = []
+  console.log(data);
 
   return (
     <div className={styles.container}>
@@ -12,12 +13,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {!robots.length ? (<h1>Loading</h1>) : (
-        <div>
-          <h1>RoboFriends</h1>
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
         </div>
       )}
-
-
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  // console.log(res);
+
+  return {
+    props: { data }
+  }
 }
